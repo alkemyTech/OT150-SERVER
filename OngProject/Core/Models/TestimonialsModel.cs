db5900530856926b4b018be92e1b412bf31f9890
JuanRomanRiquelme010 <santiagoezequielmartinez03@gmail.com>
2022-02-17 08:46:25 -0300

    Creación de Testimonials Model

---
+++ OngProject/Core/Models/TestimonialsModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace OngProject.Core.Models
+{
+    public class TestimonialsModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(255)]
+        [Required]
+        public string Name { get; set; }
+        [MaxLength(65535)]
+        public string Content { get; set; }
+        [MaxLength(255)]
+        public string Image { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DeletedAt { get; set; }
+    }
+}
