86a5816c1b21cfe94b5826368a2c6c93b591256a
alanrios21 <riosalan264@gmail.com>
2022-02-17 17:36:42 -0300

    Creacion de RoleModel

---
+++ OngProject/Core/Models/RoleModel.cs
@@ -0,0 +1,20 @@
+﻿using OngProject.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Core.Models
+{
+    public class RoleModel : EntityBase
+    {
+      
+        [Required]
+       
+        public string NameRole { get; set; }
+        public string DescriptionRole { get; set; }
+
+
+    }
+}
