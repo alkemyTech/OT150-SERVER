587c1824556fc163a7434c54ee50c21af4159c3d
alanrios21 <riosalan264@gmail.com>
2022-02-17 17:39:34 -0300

    Creacion de CategorierModel

---
+++ OngProject/Core/Models/CategorieModel.cs
@@ -0,0 +1,21 @@
+﻿using OngProject.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Core.Models
+{
+    public class CategorieModel : EntityBase
+    {
+        [Required]
+        public string NameCategorie { get; set; }
+        [Required]
+        public string DescriptionCategorie { get; set; }
+        [Required]
+        public string Image{ get; set; }
+       
+        
+    }
+}
