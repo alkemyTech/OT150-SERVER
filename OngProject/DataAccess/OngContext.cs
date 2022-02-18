4571de7c16f5138529e8623a5f84be03067a7927
julieta sosa <juli20002010@live.com>
2022-02-18 16:01:49 -0300

    create OngContext

---
+++ OngProject/DataAccess/OngContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using OngProject.Entities;
+
+namespace OngProject.DataAccess
+{
+    public class OngContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public OngContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer(_configuration["ConnectionStrings:Challenge"]);
+        }
+
+        public DbSet<ContactsModel> ContactsModel { get; set; }
+        public DbSet<TestimonialsModel> TestimonialsModel { get; set; }
+
+    }
+}
