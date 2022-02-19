ce9d46f012df5c1e74bc62f033fdb7f8f3cc570f
Nicolas Castillo <nicoecas.96@gmail.com>
2022-02-19 05:15:52 -0300

    Creación del modelo Comentario

---
+++ OngProject/Entities/CommentModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OngProject.Entities
+{
+    public class CommentModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(65535)]
+        public string Body { get; set; }
+
+        [ForeignKey("NewsModel")]
+        public int News_Id { get; set; }
+        public virtual NewsModel News { get; set; }
+
+        [ForeignKey("UserModel")]
+        public int User_Id { get; set; }
+        public virtual UserModel User { get; set; }
+    }
+}
