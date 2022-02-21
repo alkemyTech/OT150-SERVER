c0aff4a378216f00c909d81c2c2eb3f2d4d1581e
Nicolas Castillo <nicoecas.96@gmail.com>
2022-02-21 04:29:31 -0300

    Activity Model

---
+++ OngProject/Entities/ActivityModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OngProject.Entities
+{
+    public class ActivityModel : EntityBase
+    {
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(65535)]
+        public string Content { get; set; }
+        [Required]
+        [MaxLength(255)]
+        public byte[] Image { get; set; }
+    }
+}
