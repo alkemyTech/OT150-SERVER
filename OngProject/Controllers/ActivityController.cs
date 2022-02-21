512c98a842e582d04fab373bba20448f4396b785
julieta sosa <juli20002010@live.com>
2022-02-21 10:16:48 -0300

    create activity controller

---
+++ OngProject/Controllers/ActivityController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OngProject.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActivityController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ActivityController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+    }
+}
