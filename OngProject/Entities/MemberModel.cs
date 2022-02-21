40681aedd90d5d5c37ee46a240d5742250962e1d
julieta sosa <juli20002010@live.com>
2022-02-21 17:34:44 -0300

    move entities from models folder to entities folder fixing namespace

--- OngProject/Entities/MemberModel.cs
+++ OngProject/Entities/MemberModel.cs
@@ -1,8 +1,7 @@
-﻿using OngProject.Entities;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace OngProject.Core.Models
+namespace OngProject.Entities
 {
     public class MemberModel : EntityBase
     {
---
+++ OngProject/Entities/RoleModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OngProject.Entities
+{
+    public class RoleModel : EntityBase
+    {
+      
+        [Required]
+       
+        public string NameRole { get; set; }
+        public string DescriptionRole { get; set; }
+
+
+    }
+}
