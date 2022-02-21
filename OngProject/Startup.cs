5660d2e9b639d010b716e8e2440c3f81270b8621
Santiago Martinez <79525736+MartinezSantiago@users.noreply.github.com>
2022-02-21 17:06:28 -0300

    Update Startup.cs

--- OngProject/Startup.cs
+++ OngProject/Startup.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
-using OngProject.Configuration;
+using OngProject.Core.Models;
 using OngProject.DataAccess;
 using OngProject.Repositories;
 using OngProject.Repositories.Interfaces;
