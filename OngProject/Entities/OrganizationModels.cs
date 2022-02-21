9ed8a25bccbd81c2c56e82cb17444a084cdf44cc
arielPerez447 <64883289+ArielEzequielPerez@users.noreply.github.com>
2022-02-21 11:58:10 -0300

    cambie de ubicacion de el modelo organization de models a entity

---
+++ OngProject/Entities/OrganizationModels.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using OngProject.Entities;
+
+namespace OngProject.Core.Models
+{
+    public class OrganizationModels : EntityBase
+    {
+        [Required(ErrorMessage = "Entered a name")]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        public string Image { get; set; }
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+
+        [Display(Name = "Phone")]
+        public int Phone { get; set; }
+
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Entered text")]
+        [Display(Name = "Welcome Text")]
+        public string WelcomeText { get; set; }
+
+        [Required(ErrorMessage = "Entered text")]
+        [Display(Name = "About Us Text")]
+        public string AboutUsText { get; set; }
+    }
+}
