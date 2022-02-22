dd851641ff5bcb3928510f24a2deef98af68b4a8
JC-ORBEZO <jose.orbezo@alumnos.frgp.utn.edu.ar>
2022-02-22 12:01:19 -0300

    Crear repositorio, controlador y servicios de News.

---
+++ OngProject/Controllers/NewsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OngProject.Repositories.Interfaces;
+
+namespace OngProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NewsController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public NewsController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+    }
+}
