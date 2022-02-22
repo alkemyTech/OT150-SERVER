e6275a54b916112159c15a408829536584e84154
alanrios21 <riosalan264@gmail.com>
2022-02-21 22:06:07 -0300

    Creacion de UserModel

--- OngProject/Core/Models/UserModel.cs
+++ OngProject/Core/Models/UserModel.cs
@@ -1,11 +1,28 @@
-﻿using System;
+﻿using OngProject.Entities;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace OngProject.Core.Models
 {
-    public class UserModel
+    public class UserModel : EntityBase
     {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string Photo { get; set; }
+        [ForeignKey("RolModel")]
+        public int RoleId { get; set; }
+
+
     }
 }
