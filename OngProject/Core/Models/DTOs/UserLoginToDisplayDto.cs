3500ad80a41166ced7d1e8f0c3ecf4f7fc7ab4dc
julieta sosa <juli20002010@live.com>
2022-02-25 19:26:22 -0300

    create userlogindto's

---
+++ OngProject/Core/Models/DTOs/UserLoginToDisplayDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OngProject.Core.Models.DTOs
+{
+    public class UserLoginToDisplayDto
+    {
+        [Required]
+        [MaxLength(255, ErrorMessage = "The maximum length is 255 letters")]
+
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(255, ErrorMessage = "The maximum length is 255 letters")]
+        public string LastName { get; set; }
+        [Required]
+        [EmailAddress]
+        [MaxLength(320, ErrorMessage = "The maximum length is 320 letters")]
+        public string Email { get; set; }
+
+    }
+}
