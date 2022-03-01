82fdd26db545137a3ff021f5499b6fecf9d306b4
Guillermo Orue Marighetti <59489394+ggomarighetti@users.noreply.github.com>
2022-03-01 18:17:47 -0300

    Created "comments" controller.

---
+++ OngProject/Controllers/CommentsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OngProject.Core.Interfaces;
+using System.Threading.Tasks;
+
+namespace OngProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentsController : ControllerBase
+    {
+        private readonly ICommentBusiness _commentBusiness;
+
+        public CommentsController(ICommentBusiness commentBusiness)
+        {
+            _commentBusiness = commentBusiness;
+        }
+
+        [HttpGet]
+        public IActionResult GetComments()
+        {
+            try
+            {
+
+                return Ok(_commentBusiness.GetComments());
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
