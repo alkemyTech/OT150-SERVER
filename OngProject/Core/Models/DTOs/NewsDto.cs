4cd3aff1c14bea4c882da49dad5cfff3be2607ee
Guillermo Orue Marighetti <59489394+ggomarighetti@users.noreply.github.com>
2022-03-01 18:56:54 -0300

    Created a dto for show news

---
+++ OngProject/Core/Models/DTOs/NewsDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OngProject.Core.Models.DTOs
+{
+    public class NewsDto
+    {
+        [Required(ErrorMessage = "entered a name")]
+        [Display(Name = "Name")]
+        [MaxLength(100)]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "entered content")]
+        [Display(Name = "Content")]
+        public string Content { get; set; }
+        [Required]
+        public string Image { get; set; }
+    }
+}
