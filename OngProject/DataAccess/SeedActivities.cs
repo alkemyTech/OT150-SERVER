4d616d0355ce2132e42b5f710960443f516c0d24
JC-ORBEZO <jose.orbezo@alumnos.frgp.utn.edu.ar>
2022-03-01 23:43:58 -0300

    Feature/US-60 Crear Model Seed Data para guardar Actividades.

---
+++ OngProject/DataAccess/SeedActivities.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OngProject.Entities;
+using System;
+
+namespace OngProject.DataAccess
+{
+    public class SeedActivities : IEntityTypeConfiguration<ActivityModel>
+    {
+		public void Configure(EntityTypeBuilder<ActivityModel> builder)
+		{
+			for (int i = 1; i < 11; i++)
+			{
+				builder.HasData(
+					new ActivityModel
+					{
+						Id = i,
+						Name = "activity " + i,
+						Content = "Contenido de actividad " + i,
+						Image = "Image from activity " + i,
+						LastModified = DateTime.Now,
+						SoftDelete = true
+					}
+				);
+			}
+		}
+	}
+}
