b59c569fc1d7410a1c03896462b0e754d4ab5dbb
Nicolas Castillo <nicoecas.96@gmail.com>
2022-03-02 14:56:47 -0300

    Seed Organization

--- OngProject/DataAccess/SeedOrganization.cs
+++ OngProject/DataAccess/SeedOrganization.cs
@@ -1,6 +1,33 @@
-﻿namespace OngProject.DataAccess
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OngProject.Entities;
+using System;
+
+
+namespace OngProject.DataAccess
 {
-    public class SeedOrganization
-    {
-    }
+    public class SeedOrganization : IEntityTypeConfiguration<OrganizationModel>
+	{
+		public void Configure(EntityTypeBuilder<OrganizationModel> builder)
+		{
+			builder.HasData(
+				new OrganizationModel
+				{
+					Id = 1,
+					Name = "Organization",
+					Image = "Image from organization",
+					Address = "Address of Organization",
+					Phone = 1112345678,
+					Email = "Organization@ong.com",
+					WelcomeText = "Welcome to Organization",
+					AboutUsText = "We are Organization...",
+					FacebooK = "OrganizationFaceBook",
+					Linkedin = "OrganizationLinkedin",
+					Instagram = "OrganizationInstagram",
+					LastModified = DateTime.Now,
+					SoftDelete = true
+				}
+			);
+		}
+	}
 }
