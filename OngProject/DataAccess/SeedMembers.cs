8b8d067eefc80af10ee38c3cb68407a53e7f6d35
julieta sosa <juli20002010@live.com>
2022-03-02 10:32:48 -0300

    create seed data model for members entity

---
+++ OngProject/DataAccess/SeedMembers.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OngProject.Entities;
+using System;
+
+namespace OngProject.DataAccess
+{
+    public class SeedMembers : IEntityTypeConfiguration<MemberModel>
+	{
+		public void Configure(EntityTypeBuilder<MemberModel> builder)
+		{
+			for (int i = 1; i < 11; i++)
+			{
+				builder.HasData(
+					new MemberModel
+					{
+						Id = i,
+						Name = "Member " + i,
+						Description = "Description from member " + i,
+						Image = "Image from member " + i,
+						LastModified = DateTime.Now,
+						SoftDelete = true
+					}
+				);
+			}
+		}
+	}
+}
