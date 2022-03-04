f40c604934a084c5af519266cbc82e2c6a04347d
julieta sosa <juli20002010@live.com>
2022-03-03 22:33:14 -0300

    delete password property and change string role to int roleId

--- OngProject/Core/Models/TokenParameter.cs
+++ OngProject/Core/Models/TokenParameter.cs
@@ -6,7 +6,6 @@ namespace OngProject.Core.Models
     {
         public int Id { get; set; }
         public string Email { get ; set; }
-        public string Password { get ; set; }
-        public string Role { get; set ; }
+        public int RoleId { get; set ; }
     }
 }
