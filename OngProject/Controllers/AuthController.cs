f5d19ec0753ff8b036918ed8a536a00dd1dcbcd9
julieta sosa <juli20002010@live.com>
2022-03-03 22:40:40 -0300

    add GenerateJwtToken method to Login endpoint

--- OngProject/Controllers/AuthController.cs
+++ OngProject/Controllers/AuthController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using OngProject.Core.Interfaces;
+using OngProject.Core.Models;
 using OngProject.Core.Models.DTOs;
 using System.Threading.Tasks;
 
@@ -9,12 +10,14 @@ namespace OngProject.Controllers
     {
         private readonly IUserBusiness _userBusiness;
         private readonly IEmailBusiness _emailBusiness;
+        private readonly IJwtHelper _jwtHelper;
 
 
-        public AuthController(IUserBusiness userBusiness, IEmailBusiness emailBusiness)
+        public AuthController(IUserBusiness userBusiness, IEmailBusiness emailBusiness, IJwtHelper jwtHelper)
         {
             this._userBusiness = userBusiness;
             _emailBusiness = emailBusiness;
+            _jwtHelper = jwtHelper;
         }
 
         [HttpPost("Register")]
@@ -56,7 +59,17 @@ namespace OngProject.Controllers
                 {
                     return BadRequest("Error: Email or password are incorrect");
                 }
-                return Ok(user);
+
+                var tokenParameter = new TokenParameter
+                {
+                    Email = user.Email,
+                    Id = user.Id,
+                    RoleId = user.RoleId
+                };
+
+                var token = _jwtHelper.GenerateJwtToken(tokenParameter);
+
+                return Ok(token);
                 
             }
             else
