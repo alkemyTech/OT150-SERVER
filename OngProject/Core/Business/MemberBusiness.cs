3e86317f4df5e2e7f7993864bf9675afef5825fe
JuanRomanRiquelme010 <santiagoezequielmartinez03@gmail.com>
2022-03-07 10:50:53 -0300

    DeleteMember

--- OngProject/Core/Business/MemberBusiness.cs
+++ OngProject/Core/Business/MemberBusiness.cs
@@ -1,7 +1,11 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using OngProject.Core.Interfaces;
 using OngProject.Core.Mapper;
+using OngProject.Core.Models;
 using OngProject.Core.Models.DTOs;
+
 using OngProject.Repositories.Interfaces;
 
 namespace OngProject.Core.Business
@@ -27,6 +31,44 @@ namespace OngProject.Core.Business
             }
             
             return membersDto;
+        }
+        public async Task<Response<MemberDeleteDto>> Delete(int id)
+        {
+            var members = _unitOfWork.MemberModelRepository.GetAll();
+   
+            Response<MemberDeleteDto> response = new Response<MemberDeleteDto>();
+
+            var member = members.FirstOrDefault(x=>x.Id==id);
+
+            if (member== null || member.SoftDelete==false)
+            {
+                var error = new List<string>();
+                error.Add("The member does not exist");
+                response.Data = null;
+                response.Succeeded = false;
+                response.Message = "The member has not been successfully deleted";
+                response.Errors = error.ToArray();
+
+            }
+            else
+            {
+                var memberDeleted = await _unitOfWork.MemberModelRepository.Delete(id);
+                _unitOfWork.SaveChanges();
+                response.Data = entityMapper.MemberModelToMemberDeleteDto(memberDeleted);
+                response.Succeeded = true;
+                response.Message = "The member has been successfully deleted";
+
+            }
+           
+            
+            return response;
+            
+
+
+
+         
+
+
         }
 
     }
--- OngProject/Core/Interfaces/IMembers.cs
+++ OngProject/Core/Interfaces/IMembers.cs
@@ -1,3 +1,4 @@
+using OngProject.Core.Models;
 using OngProject.Core.Models.DTOs;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -7,6 +8,6 @@ namespace OngProject.Core.Interfaces
     public interface IMembers
     {
         IEnumerable<MemberDto> GetMembers();
-
+        Task<Response<MemberDeleteDto>> Delete(int id);
     }
 }
