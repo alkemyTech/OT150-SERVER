4078dab64d9a4703f2eae533a96f1daab8e171ae
julieta sosa <juli20002010@live.com>
2022-03-13 17:33:40 -0300

    modify Get endpoint for members

--- OngProject/Controllers/MemberController.cs
+++ OngProject/Controllers/MemberController.cs
@@ -1,6 +1,11 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using OngProject.Core.Interfaces;
+using OngProject.Core.Models;
+using OngProject.Core.Models.DTOs;
+using OngProject.Repositories.Interfaces;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace OngProject.Controllers
@@ -8,19 +13,43 @@ namespace OngProject.Controllers
     public class MemberController : ControllerBase
     {
         private readonly IMembers _members;
+        private readonly IUriService _uriService;
 
-        public MemberController(IMembers member)
+        public MemberController(IMembers member, IUriService uriService)
         {
             _members = member;
+            _uriService = uriService;
 
         }
         
         [Authorize(Roles = "Admin")]
         [HttpGet("Members")]
-        public IActionResult GetMembers()
+        public IActionResult GetMembers([FromQuery] PaginationParams paginationParams)
         {
-            return Ok(_members.GetMembers());
+            var member = _members.GetMembers(paginationParams);
+
+            var metadata = new Metadata
+            {
+                TotalCount = member.TotalCount,
+                PageSize = member.PageSize,
+                CurrentPage = member.CurrentPage,
+                TotalPages = member.TotalPages,
+                HasNextPage = member.HasNextPage,
+                HasPreviousPage = member.HasPreviousPage,
+                NextPageUrl = _uriService.GetNextPage(paginationParams, "Members").ToString(),
+                PreviousPageUrl = _uriService.GetPreviousPage(paginationParams, "Members").ToString()
+            };
+
+            var response = new PaginationResponse<IEnumerable<MemberDto>>(member)
+            {
+                Meta = metadata
+            };
+
+            Response.Headers.Add("X-Pagination", JsonConvert.SerializeObject(metadata));
+
+            return Ok(response);
         }
+
         [Authorize]
         [HttpDelete("Members/{id:int}")]
 
