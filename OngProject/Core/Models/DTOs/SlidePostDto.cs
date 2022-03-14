20e9797affb0087d28f57ab3da5f49394214616a
JuanRomanRiquelme010 <santiagoezequielmartinez03@gmail.com>
2022-03-14 20:18:45 -0300

    PostSlides

--- OngProject/Core/Models/DTOs/SlidePostDto.cs
+++ OngProject/Core/Models/DTOs/SlidePostDto.cs
@@ -5,7 +5,7 @@ namespace OngProject.Core.Models.DTOs
     public class SlidePostDto
     {
         [Required]
-        [MaxLength(32767, ErrorMessage = "32767 is the maximum length.")]
+ 
         public string Image{ get; set; }
         [Required]
         [MaxLength(255,ErrorMessage="255 is the maximum length.")]
