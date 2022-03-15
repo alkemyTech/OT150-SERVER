497111d3b799f3d97cf076f8b3c26521924b7560
julieta sosa <juli20002010@live.com>
2022-03-14 21:15:25 -0300

    fix formatting

--- OngProject/Controllers/UserController.cs
+++ OngProject/Controllers/UserController.cs
@@ -1,6 +1,4 @@
-﻿using Amazon.S3;
-using Amazon.S3.Model;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -8,9 +6,6 @@ using OngProject.Core.Business;
 using OngProject.Core.Interfaces;
 using OngProject.Core.Models;
 using OngProject.Core.Models.DTOs;
-using OngProject.Repositories.Interfaces;
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -19,21 +14,15 @@ namespace OngProject.Controllers
 {
     public class UserController : Controller
     {
-
-
-
         private readonly IUserBusiness _userBusiness;
         private readonly ImagesBusiness _imagesBusiness;
         private readonly IConfiguration _configuration;
 
         public UserController(IUserBusiness userBusiness, IConfiguration configuration)
         {
-
             _userBusiness = userBusiness;
             _imagesBusiness = new ImagesBusiness(configuration);
             _configuration = configuration;
-
-
         }
 
         [HttpGet("Lista")]
@@ -49,8 +38,7 @@ namespace OngProject.Controllers
             {
                 return BadRequest();
             }
-        }
-      
+        }      
 
         [HttpPost]
         [Route("image")]
@@ -60,7 +48,6 @@ namespace OngProject.Controllers
             var response = await _imagesBusiness.UploadFileAsync(image);
             return Ok(response);
         }
-
 
         [Authorize]
         [HttpDelete("users")]
