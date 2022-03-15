a34938b9e5d92d07d0396a84cadb053f4bcb24ac
julieta sosa <juli20002010@live.com>
2022-03-14 21:14:59 -0300

    fix formatting

--- OngProject/Core/Models/DTOs/MemberDto.cs
+++ OngProject/Core/Models/DTOs/MemberDto.cs
@@ -3,17 +3,10 @@
     public class MemberDto
     {
         public string Name { get; set; }
-
         public string FacebookUrl { get; set; }
-
-        public string InstagramUrl { get; set; }
-
-  
-        public string LinkedinUrl { get; set; }
-
-        
+        public string InstagramUrl { get; set; } 
+        public string LinkedinUrl { get; set; }       
         public string Image { get; set; }
-
         public string Description { get; set; }
     }
 }
